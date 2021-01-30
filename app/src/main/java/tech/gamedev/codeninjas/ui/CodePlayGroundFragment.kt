961513f6fe4475ff30ebc289c81ed1e317dab36f
godfella777 <ybenhaim777@gmail.com>
2021-01-31 01:40:35 +0200

    Added CodePlayGroundFragment.kt Added CodePlay Icon to action bar

---
+++ app/src/main/java/tech/gamedev/codeninjas/ui/CodePlayGroundFragment.kt
@@ -0,0 +1,13 @@
+package tech.gamedev.codeninjas.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import tech.gamedev.codeninjas.R
+
+
+class CodePlayGroundFragment : Fragment(R.layout.fragment_code_play_ground) {
+
+}
